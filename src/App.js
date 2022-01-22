c7b1bebbb375e0b73c4285ac4bf810f4d138b22c
himelsaha29 <himelsaha29@live.com>
2022-01-21 22:45:51 -0500

    New logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="230" height="auto"/>
         <p>
           Airbus A350 customized home page coming soon
         </p>
