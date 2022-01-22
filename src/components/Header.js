2eb78900c785687dd8bceaee28f08583c94b2562
himelsaha29 <himelsaha29@live.com>
2022-01-22 11:55:25 -0500

    Header items added

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,18 @@ const Header = () => {
             <div className='header_logo'>
                 <img src={logo} className="Airbus-logo" alt="Airbus-logo" width="230" height="auto"/>
             </div>
+
+            <div className='header_center'>
+                <p>A350-800</p>
+                <p>A350-900</p>
+                <p>A350-1000XWB</p>
+            </div>
             
+            <div className='header_right'>
+                <p>Specifications</p>
+                <p>Airbus account</p>
+            </div>
+
         </div>
     )
 }
