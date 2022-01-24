aa6ffb305ab10fa0cf992c6ef9dbb70646cc0bb3
himelsaha29 <himelsaha29@live.com>
2022-01-24 12:19:01 -0500

    Adding items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app_itemsContainer">
+        <Item 
+          title="title"
+          desc="desc"
+          descLink=""
+          backgroundImg={}
+          leftBtnTxt="Order now"
+          leftBtnLink=""
+          rightBtnTxt="learn more"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+
+
+
+
+
+      </div>
       
     </div>
   );
--- src/components/Items.js
+++ src/components/Items.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Items = () => {
     return (
-        <div>
-            
+        <div className="item" style={{}}>
+
+            <div className="item_container">
+                <div className="item_text">
+                    <p>Title</p>
+                    <div className="item_textDesc">
+                        <p>Desc</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="item_lowerThird">
+                <div className="item_buttons">
+                    
+                </div>
+            </div>
+
         </div>
     )
 }
