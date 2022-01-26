2cd14377a0af66f9dc504d9a8f34c06df440425b
himelsaha29 <himelsaha29@live.com>
2022-01-26 13:20:55 -0500

    Button text centered

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import './Button.css'
 
 const Button = ({imp, text, link}) => {
     return (
-        <div className={`button ${imp == 'secondary' ? 'button_white' : ''}`}>
+        <div className={`button ${imp === 'secondary' ? 'button_white' : ''}`}>
             <a href={link}>
                 {text}
             </a>
